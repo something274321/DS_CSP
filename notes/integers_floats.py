9a538cf02069977a44987979f2626454a242bc75
something274321 <daniel.spell@ucas-edu.net>
2026-01-23 13:44:53 -0700

    floats and int notes

---
+++ notes/integers_floats.py
@@ -0,0 +1,10 @@
+"""price = float(input("How much did it cost: $"))
+tax_rate = float(input("what percent is the tax: %"))/100
+total_price = price* (1+tax_rate)
+print(round(total_price,2))"""
+
+
+
+apples = int(input("How many apples are there: "))
+friends = 12
+print("Each friend gets",apples//friends,"and I have",apples%friends,"left over")
