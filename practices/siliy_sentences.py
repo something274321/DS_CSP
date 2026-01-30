1987d179d881d385f54932e2ba072942267109f9
something274321 <daniel.spell@ucas-edu.net>
2026-01-30 23:19:31 +0000

    silly

---
+++ practices/siliy_sentences.py
@@ -0,0 +1,5 @@
+"""DJ p6 Silly Sentences"""
+verb = input("tell me a past-tense verb ")
+noun = input("tell me a noun ")
+number = input("tell me a number ")
+print(f"I ate a {noun} and then {verb} {number} times")
