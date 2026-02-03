6790492a1442916cd2b9bf75e44db07da6733551
something274321 <daniel.spell@ucas-edu.net>
2026-02-03 13:27:30 -0700

    fizzbuzz

---
+++ practices/FizzBuzz.py
@@ -0,0 +1,10 @@
+#DJ p6, FizzBuzz
+for num in range(1,51,1):
+    if num%3==0 and num%5==0:
+        print("FizzBuzz")
+    elif num%3==0:
+        print("Fizz")
+    elif num%5==0:
+        print("Buzz")
+    else:
+        print(num)
