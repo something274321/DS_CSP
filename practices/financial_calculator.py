d103f6263e767249505a13448d890125bb5548b9
something274321 <daniel.spell@ucas-edu.net>
2026-02-05 13:46:49 -0700

    update

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -1,13 +1,18 @@
-#DJ Financial Calculator
-income = int(input("what is your monthly income $ "))
-rent = int(input("what is your monthly rent $ "))
-utilities = int(input("what is your monthly utilities cost $ "))
-groceries = int(input("what is your monthly groceries cost $ "))
-transportation = int(input("what is your monthly transportation cost $ "))
-print(f"Your rent is ${rent} and that is {rent/income*100}% of your income.")
-print(f"Your utilities cost is ${utilities} and that is {utilities/income*100}% of your income.")
-print(f"Your groceries cost is ${groceries} and that is {groceries/income*100}% of your income.")
-print(f"Your transportation cost is ${transportation} and that is {transportation/income*100}% of your income.")
+#DJ Financial Calculator 2
+def money(cost):
+    return float(input(f"what is your monthly {cost} $ "))
+income = money("income")
+rent = money("rent")
+utilities = money("utilities cost")
+groceries = money("groceries cost")
+transportation = money("transportation cost")
+
+def percent(thing,cost2):
+    print(f"Your {thing} is ${cost2} and that is {cost2/income*100}% of your income.")
+percent("rent",rent)
+percent("utilities cost",utilities)
+percent("groceries cost",groceries)
+percent("transportation cost",transportation)
 life_cost = rent+utilities+groceries+transportation
 remainder = income - life_cost
 if remainder == 0:
@@ -16,5 +21,8 @@ elif remainder < 0:
     print(f"You are ${remainder*-1} in debt")
 else:
     savings = income/10
-    print(f"You should save ${savings} a month, that is 10% of your income.")
-    print (f"You have ${remainder-savings} of spending money each month!")
+    if savings > remainder:
+        print(f"You should save ${remainder} a month, that is the rest of your income.")
+    else:
+        print(f"You should save ${savings} a month, that is 10% of your income.")
+        print (f"You have ${remainder-savings} of spending money each month!")
