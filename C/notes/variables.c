659f8f3be28602617fb35725922991c80828bb06
something274321 <daniel.spell@ucsa-edu.net>
2026-02-19 13:17:24 -0700

    55425

--- C/notes/variables.c
+++ C/notes/variables.c
@@ -3,13 +3,20 @@
 
  int main(){
     int number = 8;
-    float pi = 3.14;
+    const float pi = 3.14;
     char grade = 'A'; // single char uses single quotes
-    char name[] = "Jake"; 
+    char name[] = "Jake";
+    char user[50];
+
+    printf("Tell me your name\n");
+    scanf("%s", &user);
+    
+    printf("Tell me how old you are\n");
+    scanf("%d",&number);
 
     printf("Working!\n");
     printf("%d\n", number);
     printf("The digits of pi are : %f\n", pi);
-    printf("Your name is %s you are %d old. You have an %c in programming. Pi is %f\n",name,number,grade,pi);
+    printf("Your name is %s you are %d old. You have an %c in programming. Pi is %f\n",user,number,grade,pi);
     return 0;
  }
