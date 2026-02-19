eaa6f6380a24099ba847a4268070d55626aedb77
something274321 <daniel.spell@ucsa-edu.net>
2026-02-19 13:34:37 -0700

    765765765

---
+++ C/practices/variables_practice.c
@@ -0,0 +1,18 @@
+// DS 6 variables practice
+#include <stdio.h>
+
+ int main(){
+    char name[] = "Carl";
+    int number = 3;
+    int big_number = 654;
+    char breakfast[] = "eggrolls";
+    char best_color[] = "red white an' blue";
+    char school[] = "'merica 'cadamy";
+    int year = 2026;
+    char eye[] = "brown";
+    int age = 42;
+    char fave_sub[] = "guns";
+
+    printf(" %s\n %d\n %d\n %s\n %s\n %s\n %d\n %s\n %d\n %s\n",name,number,big_number,breakfast,best_color,school,year,eye,age,fave_sub);
+    return 0;
+ }
