a8622fca70599b009e304766a21334649eddca5b
something274321 <daniel.spell@ucsa-edu.net>
2026-02-19 13:43:44 -0700

    76764

---
+++ C/practices/first_program.c
@@ -0,0 +1,9 @@
+// DS First program
+#include <stdio.h>
+
+int main(){
+    char jazz[] = input("BARRY: ya like jazz ");
+    printf("YOU: can I have money");
+    printf("BARRY: %s", jazz);
+    return 0;
+}
