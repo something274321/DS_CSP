ed7b305a47756c40e7d91b9322e6eb394c1023d2
something274321 <daniel.spell@ucsa-edu.net>
2026-03-04 13:44:34 -0700

    fizzbuzz

--- C/notes/arrays.c
+++ C/notes/arrays.c
@@ -1,12 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 int main(){
     //1
     int grades[]={84,43,99,74,2,88,92,85,97,62,81,100,96,83};
-    printf("%d\n",grades[3]);
-    grades[3] = 1;
-    printf("%d\n", grades[3]);
+    printf("%d\n",grades[2]);
+    grades[2] = 1;
+    printf("%d\n", grades[2]);
+
+    for(int i =20; i>=0; i--){
+        printf("%d ",i);
+    }
+
+
+    int grade_len = sizeof(grades)/sizeof(grades[0]);
+    for (int index = 0; index < grade_len; index++){
+        printf("\n%d",grades[index] + 5);
+    }
+
+
+    printf("\n");
+
+
+    srand(time(NULL));
+    int goose = rand() % 15 +1;
+    int count = 1;
+    for(count; count < goose; count++){
+        printf("duck\n");
+    }
+    printf("GOOSE\n");
 
 
     return 0;
---
+++ C/practices/FizzBuzz.c
@@ -0,0 +1,19 @@
+// DJ p6, FizzBuzz C
+#include <stdio.h>
+int main(){
+    for(int num = 1; num <=50; num++){
+        if (num%3==0 && num%5==0){
+            printf("FizzBuzz\n");
+        }
+        else if (num%3==0){
+            printf("Fizz\n");
+        }
+        else if (num%5==0){
+            printf("Buzz\n");
+        }
+        else{
+            printf("%d\n",num);
+        }
+    }
+    return 0;
+}
